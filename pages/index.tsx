a274abe23c3bde120658d3f76ad6152f4a9c94a4
novailoveyou <novailoveyoux@gmail.com>
2021-04-19 22:07:28 +0300

    getServerSideProps & getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-const Home = () => {
+const Home = ({ events }) => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
 
 export default Home
