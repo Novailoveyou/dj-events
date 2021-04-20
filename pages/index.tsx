8306548ca49b82a26eb9f9973136418f12e16076
novailoveyou <novailoveyoux@gmail.com>
2021-04-20 09:33:06 +0300

    getServerSideProps & getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
 
 const Home = ({ events }) => {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   )
 }
@@ -14,7 +19,7 @@ export async function getStaticProps() {
   const events = await res.json()
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   }
 }
