8c6e3a905204dcd4d4d331131961512eae92ff8b
novailoveyou <novailoveyoux@gmail.com>
2021-04-20 10:56:23 +0300

    getStaticPaths

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,11 +1,49 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-const Event = () => {
+const Event = ({ evt }) => {
   return (
     <Layout title='Add New Event'>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   )
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map(evt => ({
+    params: { slug: evt.slug },
+  }))
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  }
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0],
+//     },
+//   }
+// }
 
 export default Event
