ce6e8e9e1abc169a662996a1cb1cc60248a18748
novailoveyou <novailoveyoux@gmail.com>
2021-04-25 17:28:10 +0300

    Search Component added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Search from '@/components/Search'
 import styles from '@/styles/Header.module.sass'
 
 const Header = () => {
@@ -9,6 +10,8 @@ const Header = () => {
 					<a>DJ Events</a>
 				</Link>
 			</div>
+
+			<Search />
 
 			<nav>
 				<ul>
