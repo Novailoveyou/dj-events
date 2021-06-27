691414bb7f07e052044c9a9785f5fe0cfd0d4ae8
novailoveyou <novailoveyoux@gmail.com>
2021-06-27 14:10:17 +0300

    Image Upload

--- pages/events/edit/[id].tsx
+++ pages/events/edit/[id].tsx
@@ -8,173 +8,181 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
 import Modal from '@/components/Modal'
+import ImageUpload from '@/components/ImageUpload'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.sass'
 
 const EditEventPage = ({
-	evt: { id, image, name, performers, venue, address, date, time, description },
+  evt: { id, image, name, performers, venue, address, date, time, description }
 }) => {
-	const [values, setValues] = useState({
-		name,
-		performers,
-		venue,
-		address,
-		date,
-		time,
-		description,
-	})
-	const [imagePreview, setImagePreview] = useState(
-		image ? image.formats.thumbnail.url : null
-	)
-	const [showModal, setShowModal] = useState(false)
+  const [values, setValues] = useState({
+    name,
+    performers,
+    venue,
+    address,
+    date,
+    time,
+    description
+  })
+  const [imagePreview, setImagePreview] = useState(
+    image ? image.formats.thumbnail.url : null
+  )
+  const [showModal, setShowModal] = useState(false)
 
-	const router = useRouter()
+  const router = useRouter()
 
-	const handleSubmit = async e => {
-		e.preventDefault()
+  const handleSubmit = async e => {
+    e.preventDefault()
 
-		// Validation
-		const hasEmptyFields = Object.values(values).some(el => el === '')
+    // Validation
+    const hasEmptyFields = Object.values(values).some(el => el === '')
 
-		if (hasEmptyFields) {
-			toast.error('Please fill in all fields')
-		}
+    if (hasEmptyFields) {
+      toast.error('Please fill in all fields')
+    }
 
-		const res = await fetch(`${API_URL}/events/${id}`, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(values),
-		})
+    const res = await fetch(`${API_URL}/events/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(values)
+    })
 
-		if (!res.ok) {
-			toast.error('Something went wrong')
-		} else {
-			const evt = await res.json()
-			router.push(`/events/${evt.slug}`)
-		}
-	}
+    if (!res.ok) {
+      toast.error('Something went wrong')
+    } else {
+      const evt = await res.json()
+      router.push(`/events/${evt.slug}`)
+    }
+  }
 
-	const handleInputChange = e => {
-		const { name, value } = e.target
-		setValues({ ...values, [name]: value })
-	}
+  const handleInputChange = e => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
 
-	return (
-		<Layout title='Edit New Event'>
-			<Link href='/events'>
-				<a>{'<'} Go Back</a>
-			</Link>
-			<h1>Edit Event</h1>
-			<ToastContainer />
-			<form onSubmit={handleSubmit} className={styles.form}>
-				<div className={styles.grid}>
-					<div>
-						<label htmlFor='name'>Event Name</label>
-						<input
-							type='text'
-							id='name'
-							name='name'
-							value={values.name}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor='performers'>Performers</label>
-						<input
-							type='text'
-							name='performers'
-							id='performers'
-							value={values.performers}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor='venue'>Venue</label>
-						<input
-							type='text'
-							name='venue'
-							id='venue'
-							value={values.venue}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor='address'>Address</label>
-						<input
-							type='text'
-							name='address'
-							id='address'
-							value={values.address}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor='date'>Date</label>
-						<input
-							type='date'
-							name='date'
-							id='date'
-							value={moment(values.date).format('yyyy-MM-DD')}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor='time'>Time</label>
-						<input
-							type='text'
-							name='time'
-							id='time'
-							value={values.time}
-							onChange={handleInputChange}
-						/>
-					</div>
-				</div>
-				<div>
-					<label htmlFor='description'>Event Description</label>
-					<textarea
-						name='description'
-						id='description'
-						value={values.description}
-						onChange={handleInputChange}></textarea>
-				</div>
-				<input type='submit' value='Update Event' className='btn' />
-			</form>
+  const imageUploaded = async e => {
+    const res = await fetch(`${API_URL}/events/${id}`)
+    const data = await res.json()
+    setImagePreview(data.image.formats.thumbnail.url)
+    setShowModal(false)
+  }
 
-			<h2>Event Image</h2>
-			{imagePreview ? (
-				<Image src={imagePreview} height={100} width={170} />
-			) : (
-				<div>
-					<p>No image uploaded</p>
-				</div>
-			)}
+  return (
+    <Layout title='Edit New Event'>
+      <Link href='/events'>
+        <a>{'<'} Go Back</a>
+      </Link>
+      <h1>Edit Event</h1>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor='name'>Event Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='performers'>Performers</label>
+            <input
+              type='text'
+              name='performers'
+              id='performers'
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='venue'>Venue</label>
+            <input
+              type='text'
+              name='venue'
+              id='venue'
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='address'>Address</label>
+            <input
+              type='text'
+              name='address'
+              id='address'
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='date'>Date</label>
+            <input
+              type='date'
+              name='date'
+              id='date'
+              value={moment(values.date).format('yyyy-MM-DD')}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='time'>Time</label>
+            <input
+              type='text'
+              name='time'
+              id='time'
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='description'>Event Description</label>
+          <textarea
+            name='description'
+            id='description'
+            value={values.description}
+            onChange={handleInputChange}></textarea>
+        </div>
+        <input type='submit' value='Update Event' className='btn' />
+      </form>
 
-			<div>
-				<button
-					onClick={() => setShowModal(true)}
-					className='btn-secondary btn-icon'>
-					<FaImage /> Set Image
-				</button>
-			</div>
+      <h2>Event Image</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} height={100} width={170} />
+      ) : (
+        <div>
+          <p>No image uploaded</p>
+        </div>
+      )}
 
-			<Modal show={showModal} onClose={() => setShowModal(false)}>
-				IMAGE UPLOAD
-			</Modal>
-		</Layout>
-	)
+      <div>
+        <button
+          onClick={() => setShowModal(true)}
+          className='btn-secondary btn-icon'>
+          <FaImage /> Set Image
+        </button>
+      </div>
+
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <ImageUpload evtId={id} imageUploaded={imageUploaded} />
+      </Modal>
+    </Layout>
+  )
 }
 
 export async function getServerSideProps({ params: { id } }) {
-	const res = await fetch(`${API_URL}/events/${id}`)
-	const evt = await res.json()
+  const res = await fetch(`${API_URL}/events/${id}`)
+  const evt = await res.json()
 
-	return {
-		props: {
-			evt,
-		},
-	}
+  return {
+    props: {
+      evt
+    }
+  }
 }
 
 export default EditEventPage
