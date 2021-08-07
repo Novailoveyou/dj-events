d336657c2a53a2eba4ba755bcaf1d4e6a7503b61
novailoveyou <novailoveyoux@gmail.com>
2021-08-07 13:01:22 +0300

    Register & Login Forms

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,42 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
 import Link from 'next/link'
 import Search from '@/components/Search'
 import styles from '@/styles/Header.module.sass'
 
 const Header = () => {
-	return (
-		<header className={styles.header}>
-			<div className={styles.logo}>
-				<Link href='/'>
-					<a>DJ Events</a>
-				</Link>
-			</div>
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href='/'>
+          <a>DJ Events</a>
+        </Link>
+      </div>
 
-			<Search />
+      <Search />
 
-			<nav>
-				<ul>
-					<li>
-						<Link href='/events'>
-							<a>Events</a>
-						</Link>
-					</li>
-					<li>
-						<Link href='/events/add'>
-							<a>Add Event</a>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	)
+      <nav>
+        <ul>
+          <li>
+            <Link href='/events'>
+              <a>Events</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/events/add'>
+              <a>Add Event</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/account/login'>
+              <a className='btn-secondary btn-icon'>
+                <FaSignInAlt /> Login
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
