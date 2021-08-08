8d6ed47f4d4ab471247db07f8b53ad9662c63efa
novailoveyou <novailoveyoux@gmail.com>
2021-08-08 16:04:47 +0300

    Persist Logged In User

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -14,6 +14,10 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
   const [error, setError] = useState(null)
 
+  const router = useRouter()
+
+  useEffect(() => checkUserLoggedIn(), [])
+
   // Register user
   const register = async user => {
     console.log(user)
@@ -44,8 +48,16 @@ export const AuthProvider = ({ children }) => {
   }
 
   // Check if user is logged in
-  const checkUserLoggedIn = async user => {
-    console.log('Check')
+  const checkUserLoggedIn = async () => {
+    const res = await fetch(`${NEXT_URL}/api/user`)
+    const data = await res.json()
+
+    if (res.ok) {
+      setUser(data.user)
+      router.push('/account/dashboard')
+    } else {
+      setUser(null)
+    }
   }
 
   return (
