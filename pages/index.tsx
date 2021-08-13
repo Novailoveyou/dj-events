0c6fc13c0b58f17a26fead9301076434853880b7
novailoveyou <novailoveyoux@gmail.com>
2021-08-13 23:09:24 +0300

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
 
 const Home = ({ events }) => {
-  //
   return (
     <Layout>
       <h1>Upcoming Events</h1>
